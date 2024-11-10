e5c4d059dcef1e6c33a0ee33a020ae4afe14cdb4
Zeus <MRX_ENCRYPT@proton.me>
2024-11-11 01:18:44 +0530

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,5 @@
 import { ReactTyped } from "react-typed";
+
 function Hero() {
     return (
         <>
@@ -9,15 +10,18 @@ function Hero() {
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                 }}
-                className="w-11/12 rounded-lg mx-auto h-72 border-2 border-solid border-yellow-500 grid items-center text-center shadow-lg"
+                className="w-11/12 mx-auto h-[400px] lg:h-[500px] border-4 border-solid border-yellow-500 rounded-xl grid items-center text-center shadow-2xl relative"
             >
+                <div className="absolute inset-0 bg-black bg-opacity-50 rounded-xl"></div>
+
                 <ReactTyped
                     strings={["Join Us"]}
                     typeSpeed={50}
                     backSpeed={20}
                     loop
-                    className="bg-black bg-opacity-70 text-white p-3 rounded-lg font-serif text-2xl block mx-auto mb-2 shadow-md"
+                    className="relative z-10 text-white p-5 rounded-lg font-extrabold text-4xl sm:text-5xl lg:text-6xl shadow-lg max-w-lg mx-auto"
                 />
+
                 <ReactTyped
                     strings={[
                         "Every Kick Counts: Help Us Empower Young Talent!",
@@ -28,7 +32,7 @@ function Hero() {
                     typeSpeed={20}
                     backSpeed={30}
                     loop
-                    className="text-green-400 bg-black bg-opacity-70 p-3 rounded-lg font-serif text-md italic block mx-auto shadow-md"
+                    className="relative z-10 text-yellow-400 bg-black bg-opacity-70 p-4 rounded-lg font-serif text-lg sm:text-xl lg:text-2xl italic block mx-auto shadow-lg max-w-3xl"
                 />
             </div>
         </>
